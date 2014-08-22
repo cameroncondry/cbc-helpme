edfaa8c042b6c163ed8d9e83168d655f782b0da3
Cameron Condry <ccondry@pltw.org>
2014-08-21 22:23:42 -0400

    Initial Revision.

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-hm
-==
+# hm - Help Me
 
 "Help Me" - A friend that helps you out in your time of need.
+
+Help Me is a lightweight library that provides common functionality for any JavaScript application.
+
+## Functions
+
+### Extend
+
+```js
+hm.extend(obj, obj);
+```
+
+### Merge
+
+```javascript
+hm.extend(obj, obj);
+```
+
+### ForEach
+
+```javascript
+hm.forEach(obj, callback);
+```
+
+### Trim
+
+```js
+hm.trim('value');
+```
+
+### Log
+
+```js
+hm.log(obj);
+```
+
+## Comparitors
+
+```js
+hm.isUndefined(obj);
+hm.isNull(obj);
+hm.isBoolean(obj);
+hm.isString(obj);
+hm.isNumber(obj);
+hm.isObject(obj);
+hm.isFunction(obj);
+hm.isArray(obj);
+```
---
+++ helpme.js
@@ -0,0 +1,127 @@
+/**
+ *    "Help Me" JavaScript Library v1.0.0
+ *
+ *    Copyright 2014 Cameron Condry
+ *    Released under the MIT license
+ */
+
+;(function (window) {
+    'use strict';
+
+    var hm = function () {};
+
+    hm.prototype = {
+        // comparisons
+        isUndefined: function (obj) {
+            return obj === void 0;
+        },
+        isNull: function (obj) {
+            return obj === null;
+        },
+        isBoolean: function (obj) {
+            return typeof obj === 'boolean';
+        },
+        isString: function (obj) {
+            return typeof obj === 'string';
+        },
+        isNumber: function (obj) {
+            return typeof obj === 'number';
+        },
+        isObject: function (obj) {
+            return typeof obj === 'object' && !!obj;
+        },
+        isFunction: function (obj) {
+            return typeof obj === 'function';
+        },
+
+        // array functions
+
+        // delegate to native Array.isArray
+        isArray: Array.isArray || function (obj) {
+            return toString.call(obj) === '[object Array]';
+        },
+
+        slice: Array.slice,
+
+        // object functions
+
+        // extend a given object's properties with object(s)
+        extend: function (obj) {
+            var src, copy, options, clone, i = 1;
+            var target = arguments[0] || {};
+            var length = arguments.length;
+
+            // enforce target as object
+            target = this.isObject(target) ? target : {};
+
+            // update all properties to target
+            for (; i < length; i++) {
+                options = arguments[i] || {};
+
+                for (name in options) {
+                    src = target[name];
+                    copy = options[name];
+
+                    // protect against obvious recusion
+                    if (target === copy) continue;
+
+                    // deep copy if object is encountered
+                    if (copy && this.isObject(copy)) {
+                        // extend the clone instead of original object
+                        clone = src && this.isObject(src) ? src : {};
+
+                        target[name] = this.extend(clone, copy);
+                    }
+
+                    // do not copy undefined values
+                    else if (!this.isUndefined(copy)) {
+                        target[name] = copy;
+                    }
+                }
+            }
+
+            return target;
+        },
+
+        // merge two or more objects without altering
+        merge: function (obj) {
+            var args = [{}];
+            args.push.apply(args, this.slice(arguments));
+
+            return this.extend.apply(this, args);
+        },
+
+        // collection functions
+
+        // iterate over a collection with a given callback
+        forEach: function (obj, callback) {
+            if (!this.isObject(obj) && !this.isArray(obj)) return obj;
+
+            var i, length = obj.length;
+
+            if (this.isArray(obj)) {
+                for (i = 0; i < length; i++) {
+                    callback.call(obj[i], i, obj[i]);
+                }
+            } else {
+                for (i in obj) {
+                    callback.call(obj[i], i, obj[i]);
+                }
+            }
+
+            return obj;
+        },
+
+        // helper functions
+
+        trim: function (value) {
+            return this.isString(value) ? value.trim() : value;
+        },
+        log: function (value) {
+            console.log(this.slice(arguments));
+        }
+    };
+
+    // expose library to global scope
+    window.hm = new hm();
+}(window));
